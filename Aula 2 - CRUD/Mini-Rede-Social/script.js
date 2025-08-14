eb24235b6b5b4ba8000aa5495da2dc624bc55987
Eduardo <eduardoulisses2006@gmail.com>
2025-08-14 20:53:20 -0300

    Aula 2: CRUD

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,80 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+const pessoa = {
+    nome: "Eduardo",
+    idade: 18,
+    "Cidade Natal": "São Paulo",
+    isAdmin: true
+}
+
+window.onload = function(){
+    showPosts();
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+//CREATE
+function addPost(evento) {
+    evento.preventDefault();
+
+    // Captura os dados da postagem
+    const postText = document.querySelector("#postText").value;
+    const postCategory = document.querySelector("#postCategory").value
+    const postImage = document.querySelector("#postImage").value;
+    const postData = new Date().toLocaleString()
+
+    // Cria um objeto com os dados capturados da postagem
+    const newPost = {
+        text: postText,
+        category: postCategory,
+        image: postImage,
+        date: postData
+    }
+
+    // Adiciona no começo da lista de posts
+    posts.unshift(newPost);
+    showPosts()
+}
+
+//READ
+function showPosts() {
+    // Pega o elemento HTML do espaço da lista de posts
+    const postList = document.querySelector("#postList");
+    // Deixa o espaço de posts vazio
+    postList.innerHTML = '';
+
+    // Para cada item no array "posts", cria um post
+    posts.forEach(post => {
+        const cardPost = document.createElement("div");
+        cardPost.classList.add("card-post");
+
+        if (post.image) {
+            cardPost.innerHTML = `
+            <h2>${post.text}</h2>
+            <img src=${post.image}>
+            <p>Categoria: ${post.category}</p>
+            <p>Data e Hora: ${post.date}</p>
+            `
+        } else {
+            cardPost.innerHTML = `
+            <h2>${post.text}</h2>
+            <p>Categoria: ${post.category}</p>
+            <p>Data e Hora: ${post.date}</p>
+            `     
+        }
+
+        // Adiciona o post no HTML
+        postList.appendChild(cardPost);
+    })
+}
+
+//UPDATE
+function editPost() {
+
+}
+
+//DELETE
+function deletePost() {
+    
+}
